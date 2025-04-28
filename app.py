9a13b540f68b65c5da07bdceb8cdeef8da6f36a1
jpambroggio <80980265+jpambroggio@users.noreply.github.com>
2025-04-28 13:02:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, Response, send_file
+from flask import Flask, Response
 from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app)  # <-- Esto permite CORS automáticamente
+CORS(app)
 
-ORIGINAL_STREAM_URL = "http://190.94.160.6:8081/hls/hd-live.m3u8"
+ORIGINAL_BASE_URL = "http://190.94.160.6:8081/hls"
 
 @app.route('/stream.m3u8')
 def stream_m3u8():
-    r = requests.get(ORIGINAL_STREAM_URL, stream=True)
-    return Response(r.iter_content(chunk_size=1024), content_type=r.headers['Content-Type'])
+    url = f"{ORIGINAL_BASE_URL}/hd-live.m3u8"
+    r = requests.get(url, stream=True)
+    return Response(r.iter_content(chunk_size=1024), content_type=r.headers.get('Content-Type', 'application/vnd.apple.mpegurl'))
+
+@app.route('/<path:filename>')
+def stream_ts(filename):
+    url = f"{ORIGINAL_BASE_URL}/{filename}"
+    r = requests.get(url, stream=True)
+    return Response(r.iter_content(chunk_size=1024), content_type=r.headers.get('Content-Type', 'video/MP2T'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
